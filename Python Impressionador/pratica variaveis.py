e3e2a537992943d4c0ded91765c33705bd263509
Alexandre Assis do Nascimento <alexandre7assis@gmail.com>
2024-06-11 16:47:41 -0300

    att

---
+++ Python Impressionador/pratica variaveis.py
@@ -0,0 +1,13 @@
+a = 20
+b = 30
+soma = a + b
+
+print('O resultado da soma de {} e {} é {}'.format(a, b, soma))
+print(f'O resultado da soma de {a} e {b} é {soma} ')
+
+print(f'{soma:.0f}')
+print(f'{soma:.1f}')
+print(f'{soma:.2f}')
+print(f'{soma:.3f}')
+print(f'{soma:.4f}')
+print(f'{soma:.5f}')
