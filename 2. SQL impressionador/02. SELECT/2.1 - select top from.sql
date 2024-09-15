8c47d8a0b0a9795070ce77f6670063fd1199aadd
Alexandre Assis do Nascimento <alexandre7assis@gmail.com>
2024-09-15 16:20:42 -0300

    aula 15-09-2024

--- 2. SQL impressionador/02. SELECT/2.1 - select top from.sql
+++ 2. SQL impressionador/02. SELECT/2.1 - select top from.sql
@@ -1,12 +1,12 @@
 -- Selecionando 10 primeiras linhas da coluna First Name
-SELECT TOP 10
+SELECT TOP (10)
 	FirstName
 FROM
-	ContosoRetailDW.dbo.DimCustomer;
+	DimCustomer
 
 -- Selecionando 10 primeiras linhas da coluna BirthDate
-SELECT TOP 10
+SELECT TOP (10)
 	BirthDate
 FROM
-	DimCustomer;
+	DimCustomer
 
